9d3e977eababf08b667177fcd10806dc687e5220
omateusamaral <mateus.amaral018@gmail.com>
2024-08-29 19:00:01 -0300

    test: created test for survey

---
+++ src/seeds/survey.seed.ts
@@ -0,0 +1,60 @@
+import { TargetAudience } from '../interfaces/TargetAudience';
+import { Survey } from '../survey.model';
+
+export const seedSurvey: Partial<Survey>[] = [
+  {
+    surveyId: '7f8ab2fd-9c69-48a0-ba1c-22a62665ab14',
+    title: 'title',
+    targetAudience: TargetAudience.YoungAdults,
+    starRating: 5,
+    contactEmail: 'email@gmail.com',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    surveyId: 'cef0ce83-b230-4d12-b093-775c177730bd',
+    title: 'mateus',
+    targetAudience: TargetAudience.YoungAdults,
+    starRating: 5,
+    contactEmail: 'email@gmail.com',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    surveyId: 'e4e39880-8e8b-415a-92a3-4580413814bb',
+    title: 'title',
+    targetAudience: TargetAudience.YoungAdults,
+    starRating: 5,
+    contactEmail: 'email@gmail.com',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    surveyId: 'c1f81cb2-3bf4-4af4-9330-fb1d7b589cc3',
+    title: 'mateus',
+    targetAudience: TargetAudience.YoungAdults,
+    starRating: 5,
+    contactEmail: 'mateus2@gmail.com',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    surveyId: 'bb4560f9-ad12-4028-acff-a01f173a2ca0',
+    title: 'gat2o',
+    targetAudience: TargetAudience.YoungAdults,
+    starRating: 1,
+    contactEmail: 'mateus@gmail.com',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+
+  {
+    surveyId: 'bb4560f9-ad12-4028-acff-a01f173a2ca0',
+    title: 'gat2o',
+    targetAudience: TargetAudience.OccasionShoppers,
+    starRating: 1,
+    contactEmail: 'mateus@gmail.com',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
---
+++ src/survey.service.spec.ts
@@ -0,0 +1,187 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { SurveyService } from './survey.service';
+import { Survey } from './survey.model';
+import { Repository, SelectQueryBuilder } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { PageTokenService } from './page-token.service';
+import { SurveyCreateFieldsDto } from './dtos/survey-create-fields.dto';
+import { TargetAudience } from './interfaces/TargetAudience';
+import { seedSurvey } from './seeds/survey.seed';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { SurveyPatchFieldsDto } from './dtos/survey-patch-fields.dto';
+import { OrderBy } from './interfaces/OrderBy';
+
+describe('SurveyService', () => {
+  let service: SurveyService;
+  let surveyRepository: Repository<Survey>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        SurveyService,
+        {
+          provide: getRepositoryToken(Survey),
+          useClass: Repository,
+        },
+        {
+          provide: PageTokenService,
+          useValue: {
+            encodePageToken: jest.fn(),
+            decodePageToken: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<SurveyService>(SurveyService);
+    surveyRepository = module.get<Repository<Survey>>(
+      getRepositoryToken(Survey),
+    );
+  });
+
+  describe('getSurvey', () => {
+    it('should return a survey if found', async () => {
+      //Arrange
+
+      jest
+        .spyOn(surveyRepository, 'findOneBy')
+        .mockResolvedValue(seedSurvey[0] as unknown as Survey);
+
+      //Act
+      const result = await service.getSurvey(seedSurvey[0].surveyId);
+
+      //Assert
+      expect(result).toEqual(seedSurvey[0]);
+    });
+
+    it('should throw NotFoundException if survey is not found', async () => {
+      //Arrange
+      jest.spyOn(surveyRepository, 'findOneBy').mockResolvedValue(null);
+
+      //Act && Assert
+      await expect(service.getSurvey('1')).rejects.toThrow(NotFoundException);
+    });
+  });
+  describe('createSurvey', () => {
+    it('should create a new survey and return it', async () => {
+      //Arrange
+      const surveyCreateFields: SurveyCreateFieldsDto = {
+        title: 'New Survey',
+        starRating: 4,
+        contactEmail: 'test@example.com',
+        targetAudience: TargetAudience.YoungAdults,
+      };
+
+      jest.spyOn(surveyRepository, 'insert').mockResolvedValue({
+        identifiers: [{ ...seedSurvey[0], surveyId: '1' }],
+      } as any);
+
+      jest
+        .spyOn(service, 'getSurvey')
+        .mockResolvedValue({ ...seedSurvey[0], surveyId: '1' } as Survey);
+
+      //Act
+
+      const response = await service.createSurvey(surveyCreateFields);
+
+      //Assert
+
+      expect(response).toEqual({
+        ...seedSurvey[0],
+        surveyId: '1',
+      });
+      expect(service.getSurvey).toHaveBeenCalledWith('1');
+    });
+  });
+
+  describe('patchSurvey', () => {
+    it('should update an existing survey and return it', async () => {
+      //Arrange
+      const surveyPatchFields: SurveyPatchFieldsDto = {
+        ...seedSurvey[0],
+        title: 'Updated Survey',
+      };
+
+      jest
+        .spyOn(service, 'getSurvey')
+        .mockResolvedValue(seedSurvey[0] as unknown as Survey);
+      jest.spyOn(surveyRepository, 'update').mockResolvedValue(undefined);
+
+      const result = await service.patchSurvey(
+        seedSurvey[0].surveyId,
+        surveyPatchFields,
+      );
+      expect(result).toEqual({
+        ...seedSurvey[0],
+        title: 'Updated Survey',
+      });
+      expect(service.getSurvey).toHaveBeenCalledWith(seedSurvey[0].surveyId);
+    });
+  });
+  describe('listSurveys', () => {
+    it('should return a paginated list of surveys', async () => {
+      //Arrange
+      const targetAudience = TargetAudience.FashionConsciousConsumers;
+      const starRating = 4;
+      const orderBy = OrderBy.ASC;
+      const pageSize = 2;
+      const pageToken = '';
+
+      const mockSurveys = [
+        { surveyId: '1', contactEmail: 'a@example.com' },
+        { surveyId: '2', contactEmail: 'b@example.com' },
+      ] as Survey[];
+
+      jest.spyOn(surveyRepository, 'createQueryBuilder').mockReturnValue({
+        where: jest.fn().mockReturnThis(),
+        andWhere: jest.fn().mockReturnThis(),
+        orderBy: jest.fn().mockReturnThis(),
+        addOrderBy: jest.fn().mockReturnThis(),
+        take: jest.fn().mockReturnThis(),
+        getMany: jest.fn().mockResolvedValue(mockSurveys),
+      } as unknown as SelectQueryBuilder<Survey>);
+
+      //Act
+      const result = await service.listSurveys(
+        targetAudience,
+        starRating,
+        orderBy,
+        pageSize,
+        pageToken,
+      );
+
+      //Arrange
+      expect(result.values).toEqual(mockSurveys.slice(0, pageSize));
+      expect(result.nextPageToken).toBeNull();
+    });
+
+    it('should throw BadRequestException if pageSize is invalid', async () => {
+      await expect(
+        service.listSurveys(
+          TargetAudience.OccasionShoppers,
+          4,
+          OrderBy.ASC,
+          -1,
+          '',
+        ),
+      ).rejects.toThrow(BadRequestException);
+    });
+  });
+
+  describe('listAllSurveysWithoutFilters', () => {
+    it('should return all surveys without filters', async () => {
+      //Arrange
+
+      jest
+        .spyOn(surveyRepository, 'find')
+        .mockResolvedValue(seedSurvey as unknown as Survey[]);
+
+      //Act
+      const result = await service.listAllSurveysWithoutFilters();
+
+      //Arrange
+      expect(result).toEqual(seedSurvey);
+      expect(surveyRepository.find).toHaveBeenCalledTimes(1);
+    });
+  });
+});
